7e11159f2a4259206bda34394f8c4f3ce7894936
CodySteiert <codysteiert@gmail.com>
2024-08-06 20:47:41 -0400

    new class

--- FoodAPI.py
+++ FoodAPI.py
@@ -1,15 +1,49 @@
 # Food api
 import requests
 
-# Define the endpoint
-endpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?f=a'
+class MealDB:
+    def __init__(self, letter):
+        self.endpoint = f'https://www.themealdb.com/api/json/v1/1/search.php?f={letter}'
+        self.meals = []
+        self.fetch_data()
 
-# Make the request
-response = requests.get(endpoint)
+    def fetch_data(self):
+        response = requests.get(self.endpoint)
+        if response.status_code == 200:
+            data = response.json()
+            self.parse_data(data)
+        else:
+            print(f"Error: {response.status_code}")
 
-# Check if the request was successful
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-else:
-    print(f"Error: {response.status_code}")
+    def parse_data(self, data):
+        meals = data.get('meals', [])
+        for meal in meals:
+            meal_info = {
+                'id': meal.get('idMeal'),
+                'name': meal.get('strMeal'),
+                'category': meal.get('strCategory'),
+                'area': meal.get('strArea'),
+                'instructions': meal.get('strInstructions'),
+                'thumbnail': meal.get('strMealThumb'),
+                'tags': meal.get('strTags'),
+                'youtube': meal.get('strYoutube'),
+                'ingredients': self.get_ingredients(meal)
+            }
+            self.meals.append(meal_info)
+
+    def get_ingredients(self, meal):
+        ingredients = []
+        for i in range(1, 21):
+            ingredient = meal.get(f'strIngredient{i}')
+            if ingredient:
+                ingredients.append(ingredient)
+        return ingredients
+
+    def get_meals(self):
+        return self.meals
+
+# Example Usage
+meal_db = MealDB('a')
+meals = meal_db.get_meals()
+for meal in meals:
+    print(meal)
