60a9222875ee573bfacfb9369f85ff913dbec0b2
CodySteiert <codysteiert@gmail.com>
2024-08-06 20:45:13 -0400

    start of api code

--- FoodAPI.py
+++ FoodAPI.py
@@ -1,1 +1,15 @@
 # Food api
+import requests
+
+# Define the endpoint
+endpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?f=a'
+
+# Make the request
+response = requests.get(endpoint)
+
+# Check if the request was successful
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Error: {response.status_code}")
